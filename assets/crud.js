a93a7aa166da7afc133581f730ead8d4c918e973
Pedro <p-0112@hotmail.com>
2023-02-22 22:06:56 -0600

    Cambio de variables

--- assets/crud.js
+++ assets/crud.js
@@ -1,76 +1,76 @@
-//Evento para crear un nuevo libro
+//Evento para crear un nuevo registro
 document.getElementById("formulario").addEventListener("submit", crear);
 
 // //Función crear
 function crear(e){
-    titulo = document.getElementById("titulo").value
-    descripcion = document.getElementById("descripcion").value
-    precio = document.getElementById("precio").value
+    nombreCompleto = document.getElementById("nombreCompleto").value
+    correoElectronico = document.getElementById("correoElectronico").value
+    noEmpleado = document.getElementById("noEmpleado").value
 
-    let libro = {
-        titulo,
-        descripcion,
-        precio
+    let registro = {
+        nombreCompleto,
+        correoElectronico,
+        noEmpleado
     }
 
-    if(localStorage.getItem("Libros") === null){
-        let libros = []
-        libros.push(libro)
-        localStorage.setItem("Libros", JSON.stringify(libros))
+    if(localStorage.getItem("Registros") === null){
+        let registros = []
+        registros.push(registro)
+        localStorage.setItem("Registros", JSON.stringify(registros))
     } else{
-        let libros = JSON.parse(localStorage.getItem("Libros"))
-        libros.push(libro)
-        localStorage.setItem("Libros", JSON.stringify(libros))
+        let registros = JSON.parse(localStorage.getItem("Registros"))
+        registros.push(registro)
+        localStorage.setItem("Registros", JSON.stringify(registros))
     }
     leer();
     document.getElementById("formulario").reset();
-    console.log("Libro guardado correctamente")
+    console.log("Registro guardado correctamente")
     e.preventDefault()
 }
 
 //Función leer
 function leer(){
-    let libros = JSON.parse(localStorage.getItem("Libros"));
+    let registros = JSON.parse(localStorage.getItem("Registros"));
     document.getElementById("tbody").innerHTML = ""
-    for(let i=0;i<libros.length;i++){
-        let titulo = libros[i].titulo
-        let descripcion = libros[i].descripcion
-        let precio = libros[i].precio
+    for(let i=0;i<registros.length;i++){
+        let nombreCompleto = registros[i].nombreCompleto
+        let correoElectronico = registros[i].correoElectronico
+        let noEmpleado = registros[i].noEmpleado
 
         document.getElementById("tbody").innerHTML +=
         `<tr>
-            <td>${titulo}</td>
-            <td>${descripcion}</td>
-            <td>${precio}</td>
-            <td><button onclick="eliminar('${titulo}')" class="btn btn-danger">Eliminar</button></td>
-            <td><button onclick="editar('${titulo}')" class="btn btn-success">Editar</button></td>
+            <td>${nombreCompleto}</td>
+            <td>${correoElectronico}</td>
+            <td>${noEmpleado}</td>
+            <td><button onclick="eliminar('${nombreCompleto}')" class="btn btn-danger">Eliminar</button></td>
+            <td><button onclick="editar('${nombreCompleto}')" class="btn btn-success">Editar</button></td>
         </tr>
         `
     }
 }
 
 // Función "editar"
-function editar(titulo){
-    let libros = JSON.parse(localStorage.getItem("Libros"));
-    for(let i=0; i<libros.length; i++){
-        if(libros[i].titulo === titulo){
+function editar(nombreCompleto){
+    let registros = JSON.parse(localStorage.getItem("Registros"));
+    for(let i=0; i<registros.length; i++){
+        if(registros[i].nombreCompleto === nombreCompleto){
             document.getElementById("body").innerHTML = `
-            <div class="row">
+            <div class="row d-flex justify-content-center">
             <div class="col-md-5">
                 <div class="card">
                     <div class="card-header">
-                        <h2>Editar libro</h2>
+                        <h2>Editar registro</h2>
                     </div>
                     <div class="card-body">
                         <form>
                             <div class="form-group">
-                                <input type="text" id="newtitulo" class="form-control my-3" placeholder="${libros[i].titulo}">
+                                <input type="text" id="newNombreCompleto" class="form-control my-3" placeholder="${registros[i].nombreCompleto}">
                             </div>
                             <div class="form-group">
-                                <textarea id="newdescripcion" class="form-control my-3" placeholder="${libros[i].descripcion}"></textarea>
+                                <input type="email" id="newCorreoElectronico" class="form-control my-3" placeholder="${registros[i].correoElectronico}">
                             </div>
                             <div class="form-group">
-                                <input type="number" id="newprecio" class="form-control my-3" placeholder="${libros[i].precio}">
+                                <input type="number" id="newNoEmpleado" class="form-control my-3" placeholder="${registros[i].noEmpleado}">
                             </div>
                         </form>
                         <button class="btn btn-success" onclick="actualizar('${i}')">Actualizar</button>
@@ -84,65 +84,65 @@ function editar(titulo){
 
 //Función actualizar
 function actualizar(i){
-    let libros = JSON.parse(localStorage.getItem("Libros"));
-    libros[i].titulo = document.getElementById("newtitulo").value;
-    libros[i].descripcion = document.getElementById("newdescripcion").value;
-    libros[i].precio = document.getElementById("newprecio").value;
-        localStorage.setItem("Libros",JSON.stringify(libros));
+    let registros = JSON.parse(localStorage.getItem("Registros"));
+    registros[i].nombreCompleto = document.getElementById("newNombreCompleto").value;
+    registros[i].correoElectronico = document.getElementById("newCorreoElectronico").value;
+    registros[i].noEmpleado = document.getElementById("newNoEmpleado").value;
+        localStorage.setItem("Registros",JSON.stringify(registros));
         VistaPrincipal();
 }
 
 // Función eliminar
-function eliminar(titulo){
-    let libros = JSON.parse(localStorage.getItem("Libros"));
-    for(let i=0; i<libros.length; i++){
-        if(libros[i].titulo === titulo){
-            libros.splice(i,1);
+function eliminar(nombreCompleto){
+    let registros = JSON.parse(localStorage.getItem("Registros"));
+    for(let i=0; i<registros.length; i++){
+        if(registros[i].nombreCompleto === nombreCompleto){
+            registros.splice(i,1);
         }
     }
-    localStorage.setItem("Libros", JSON.stringify(libros));
+    localStorage.setItem("Registros", JSON.stringify(registros));
     leer();
 }
 
 //Función para mostrar la interfaz principal
 function VistaPrincipal(){
-    document.getElementById("body").innerHTML= ` <div class="row">
+    document.getElementById("body").innerHTML= ` <div class="row d-flex justify-content-center">
     <div class="col-md-5">
         <div class="card">
             <div class="card-header">
-                <h2>Agregar agregar nuevo libro</h2>
+                <h2>Registrar nuevo usuario</h2>
             </div>
             <div class="card-body">
                 <form id="formulario">
                     <div class="form-group">
-                        <input type="text" id="titulo" class="form-control my-3" placeholder="Ingresa titulo">
+                        <input type="text" id="nombreCompleto" class="form-control my-3" placeholder="Ingresar Nombre completo" autofocus>
                     </div>
                     <div class="form-group">
-                        <textarea id="descripcion" class="form-control my-3" placeholder="Ingresa descripción"></textarea>
+                        <input type="email" id="correoElectronico" class="form-control my-3" placeholder="Ingresar correo electrónico">
                     </div>
                     <div class="form-group">
-                        <input type="number" id="precio" class="form-control my-3" placeholder="Ingresa precio">
+                        <input type="number" id="noEmpleado" class="form-control my-3" placeholder="Ingresar No. Empleado">
                     </div>
-                    <button type="submit" class="btn-primary">Agregar</button>
+                    <button type="submit" class="btn btn-primary">Agregar</button>
                 </form>
             </div>
         </div>
     </div>
-    <div class="col-md-6">
-        <table class="table caption-top bg-light">
+    <div class="col-md-8 my-2">
+        <table class="table caption-top table-striped">
             <thead>
-                <tr>
-                    <th scope="col">Título</th>
-                    <th scope="col">Descripción</th>
-                    <th scope="col">Precio</th>
-                </tr>
+            <tr>
+                <th scope="col">Nombre</th>
+                <th scope="col">Correo electrónico</th>
+                <th scope="col">No. Empleado</th>
+            </tr>
             </thead>
             <tbody id="tbody">
-                <tr>
-                    <td>Yo soy robot</td>
-                    <td>Libro de Sci-Fi</td>
-                    <td>200.00</td>
-                </tr>
+            <tr>
+                <td>Pedro</td>
+                <td>correo@correo.com</td>
+                <td>15975</td>
+            </tr>
             </tbody>
         </table>
     </div>
